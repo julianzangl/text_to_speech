23026e7decde01b4b347bb38270ff94ad13abafc
JuleZ_2k4 <8096@htl.rennweg.at>
2022-11-05 23:41:10 +0100

    added translation

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -5,27 +5,50 @@
     </div>
     <div class="fields">
       <div class="field-content">
-        <md-autocomplete v-model="selectedLanguage_one" :md-options="languages">
-          <label>Language</label>
-        </md-autocomplete>
+        <div class="input-mic">
+          <md-autocomplete
+            v-model="selectedLanguage_one"
+            :md-options="language_names"
+            class="lang"
+          >
+            <label>Language</label>
+          </md-autocomplete>
+          <md-icon @click.native="record_voice" class="microphone">
+            mic
+          </md-icon>
+        </div>
         <md-field class="field">
           <label>Insert text...</label>
           <md-textarea v-model="left_text" />
-          <md-icon @click="record_voice">
-            mic
-          </md-icon>
         </md-field>
+      </div>
+      <div class="translate">
+        <md-button
+          class="translate"
+          md-raised
+          md-theme="primary"
+          @click="translate"
+        >
+          <md-icon>
+            send
+          </md-icon>
+        </md-button>
       </div>
       <div class="field-content">
         <md-autocomplete
           v-model="selectedLanguage_second"
-          :md-options="languages"
+          :md-options="language_names"
+          class="lang"
         >
           <label>Language</label>
         </md-autocomplete>
         <md-field class="field">
-          <md-textarea v-model="right_text" />
-          <md-icon v-if="right_text">
+          <md-textarea v-model="right_text" :disabled="right_text == ''" />
+          <md-icon
+            v-if="right_text"
+            @click.native="text_to_speech"
+            class="speaker"
+          >
             volume_up
           </md-icon>
         </md-field>
@@ -34,6 +57,7 @@
   </div>
 </template>
 <script>
+import axios from "axios";
 export default {
   name: "Overview",
   data() {
@@ -42,24 +66,47 @@ export default {
       right_text: "",
       selectedLanguage_one: "German",
       selectedLanguage_second: "English",
-      languages: [
-        "English",
-        "French",
-        "German",
-        "Italian",
-        "Spanish",
-        "Portuguese",
-        "Russian",
-        "Chinese",
-        "Japanese",
-        "Korean",
-        "Arabic",
-      ],
+      language_names: [],
     };
   },
+
+  mounted() {
+    this.language_names = this.$store.state.languages.map((language) => {
+      return language.name;
+    });
+  },
+
   methods: {
     record_voice() {
       console.log("record_voice");
+    },
+    text_to_speech() {
+      if ("speechSynthesis" in window) {
+        let msg = new SpeechSynthesisUtterance();
+        msg.text = this.right_text;
+        msg.lang = this.getLanuguageObject(this.selectedLanguage_second).key;
+        console.log(msg.lang)
+        window.speechSynthesis.speak(msg);
+      } else {
+        alert("Sorry, your browser doesn't support text to speech!");
+      }
+    },
+
+    async translate() {
+      const response = await axios.post("https://libretranslate.de/translate", null, {
+        params: {
+          q: this.left_text,
+          source: this.getLanuguageObject(this.selectedLanguage_one).code,
+          target: this.getLanuguageObject(this.selectedLanguage_second).code,
+        },
+      });
+      this.right_text = response.data.translatedText;
+    },
+
+    getLanuguageObject(name) {
+      return this.$store.state.languages.find((language) => {
+        return language.name === name;
+      });
     },
   },
 };
@@ -68,6 +115,7 @@ export default {
 .header {
   text-align: center;
   margin-top: 20px;
+  margin-bottom: 5em;
 }
 
 .fields {
@@ -81,7 +129,44 @@ export default {
   margin: 20px;
   margin: 0 !important;
 }
+.microphone {
+  border-radius: 50%;
+  padding: 0.8em;
+  scale: 1.3;
+  margin: 0;
+  border-radius: 50%;
+  transition: all 0.6s ease;
+}
+.microphone:hover {
+  cursor: pointer;
+  box-shadow: 0 14px 28px rgba(107, 107, 107, 0.25),
+    0 5px 5px rgba(111, 111, 111, 0.22);
+}
 
-.field {
+.speaker {
+  border-radius: 50%;
+  transition: all 0.6s ease;
+}
+
+.speaker:hover {
+  cursor: pointer;
+  box-shadow: 0 14px 28px rgba(107, 107, 107, 0.25),
+    0 5px 5px rgba(111, 111, 111, 0.22);
+}
+
+.translate {
+  display: flex;
+  align-items: center;
+}
+.lang {
+  width: 40%;
+}
+
+.input-mic {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1em;
 }
 </style>
--- src/services/store.js
+++ src/services/store.js
@@ -2,33 +2,43 @@
  * Vuex store example
  */
 export default {
-    /** Application state */
-    state: {
-        counter: 0,
-    },
+  /** Application state */
+  state: {
+    languages: [
+      { key: "en-US", code: "en", name: "English" },
+      { key: "ru-RU", code: "ru", name: "Russian" },
+      { key: "de-AT", code: "de", name: "German" },
+      { key: "fr-FR", code: "fr", name: "French" },
+      { key: "es-ES", code: "es", name: "Spanish" },
+      { key: "it-IT", code: "it", name: "Italian" },
+      { key: "pt-PT", code: "pt", name: "Portuguese" },
+      { key: "zh-CN", code: "zh", name: "Chinese" },
+      { key: "ja-JP", code: "ja", name: "Japanese" },
+      { key: "ko-KR", code: "ko", name: "Korean" },
+      { key: "ar-SA", code: "ar", name: "Arabic" },
+      { key: "he-IL", code: "he", name: "Hebrew" },
+      { key: "tr-TR", code: "tr", name: "Turkish" },
+      { key: "pl-PL", code: "pl", name: "Polish" },
+      { key: "cs-CZ", code: "cs", name: "Czech" },
+    ],
+  },
 
-    /** Methods that read the application state */
-    getters: {
-        friendlyCounter(state) {
-            return `counter = ${state.counter}`
-        },
-    },
+  /** Methods that read the application state */
+  getters: {},
 
-    /** Methods that change the application state synchronously */
-    mutations: {
-        setCounter(state, counter) {
-            state.counter = counter
-        },
-
-        count(state) {
-            state.counter++
-        },
+  /** Methods that change the application state synchronously */
+  mutations: {
+    getLanguageObject(state, name) {
+      //   state.languages.forEach((language) => {
+      //     if (language.name === name) {
+      //       return language;
+      //     }
+      //   });
+        console.log(name)
+      return name;
     },
+  },
 
-    /** Methods that change the application state asynchronously */
-    actions: {
-        countLater({ commit }) {
-            setTimeout(() => { commit('count')}, 1000)
-        },
-    },
-}
+  /** Methods that change the application state asynchronously */
+  actions: {},
+};
